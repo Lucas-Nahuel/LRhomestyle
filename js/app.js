f95d5403fd1629ce4f7c294748ab73dd65116110
lucas ramirez <lucas01.ramirez@gmail.com>
2021-04-08 01:56:10 -0300

    arrays servicios

--- js/app.js
+++ js/app.js
@@ -10,15 +10,22 @@ let edadRequerida = parseInt(prompt("¿cuantos años tienes?"));
         alert("ingrese la edad porfavor");
     };
 
-    class Servicio{
-        constructor(servicio, precio){
-            this.servicio = servicio;
-            this.precio = precio;
-        }
-        hablar(){
-            console.log("elegiste el servicio " + this.servicio + ", con precio de " + this.precio);
-        }
-    };
+    // class Servicio{
+    //     constructor(servicio, precio){
+    //         this.servicio = servicio;
+    //         this.precio = precio;
+    //     }
+    //     hablar(){
+    //         console.log("elegiste el servicio " + this.servicio + ", con precio de " + this.precio);
+    //     }
+    // };
 
-    const servicio1 = new Servicio ("decoracion de lobby", 8500);
-    servicio1.hablar();
+    // const servicio1 = new Servicio ("decoracion de lobby", 8500);
+    // servicio1.hablar();
+
+    const servicios = [{ servicio: "decoracion de lobby", precio: 4500 },
+                       { servicio: "decoracion de habitacion", precio: 5000 },
+                       { servicio: "decoracion de cocina", precio: 9000} ];
+
+    const accesible = servicios.filter(servicio => servicio.precio <= 5000);
+    console.log(accesible);
